7078d68e9dbec7b825bf4c460ff6795dacb77b94
Ayaovi <djayespoiro@gmail.com>
2018-05-01 15:33:03 +0200

    Initial commit.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace bizsoft_4ss3ssm3n7.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
