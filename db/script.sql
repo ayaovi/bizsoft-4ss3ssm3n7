d299d8eed3994d75c74f469d8ec6b1e02fb1c7b7
Ayaovi <djayespoiro@gmail.com>
2018-05-04 09:42:55 +0200

    build up database script

--- db/script.sql
+++ db/script.sql
@@ -6,3 +6,37 @@ create table if not exists materials (
 	Id int(11) unsigned auto_increment primary key,
 	Description longtext
 );
+
+# retrieve table schemas.
+describe materials;
+describe items;
+describe orderline;
+describe orders;
+describe clients;
+
+# populating clients tables.
+insert into clients (FirstName, LastName) values ('John', 'Doe');
+insert into clients (FirstName, LastName) values ('John', 'Smith');
+insert into clients (FirstName, LastName) values ('Marry', 'Jane');
+insert into clients (FirstName, LastName) values ('Iker', 'Robertson');
+
+select * from clients;
+
+# populating materials tables.
+insert into materials (Description) values ('aluminium');
+insert into materials (Description) values ('copper');
+insert into materials (Description) values ('steel');
+
+select * from materials;
+
+# populating stock items tables.
+insert into items (Description) values ('table frame');
+
+select * from items;
+
+# dropping the tables.
+drop table orderline;
+drop table items;
+drop table materials;
+drop table orders;
+drop table clients;
